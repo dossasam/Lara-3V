c3de68b175a84245f3df6a6c082df86f85a90257
dossasam <newsam052@gmail.com>
2025-10-24 23:32:46 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XEIQBCYD#alaGEzeKgL1isAgbVbLHTFVrQecNKQeURNQOSOivYQw", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "aQxWCJ7K#-_y9LWSU8uvXRf7Xcg72kp5-JB1AJ0EDsBItL1sRUYE", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
